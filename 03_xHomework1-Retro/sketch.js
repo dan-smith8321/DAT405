7d1482c918fc990acd47efcf6c579820da5dd1af
Daniel Smith <danielsmithwba@Daniels-MacBook-Air-2.local>
2018-10-30 14:11:36 +0000

    updates

---
+++ 03_xHomework1-Retro/sketch.js
@@ -0,0 +1,15 @@
+//Initialization function
+function setup() {
+  console.log("Initialization: ok")
+  createCanvas(300, 200);
+}
+
+//Rendering function
+function draw() {
+  noStroke();
+  fill(78,56,41);
+    rect(80,40,10,10)
+
+  console.log("Rendering...")
+  noLoop();
+}
