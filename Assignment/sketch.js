0f554e1a83e51145c6a6d043dd6fde09982d046a
Daniel Smith <danielsmithwba@Daniels-MacBook-Air-2.local>
2018-11-12 11:15:50 +0000

    updates

--- Assignment/sketch.js
+++ Assignment/sketch.js
@@ -1,11 +1,11 @@
 var cnv;
 var song;
 var img;
-
+var x,y;
 
 function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
+  x = (windowWidth - width) / 2;
+  y = (windowHeight - height) / 2;
   cnv.position(x, y);
 }
 
@@ -14,26 +14,26 @@ function setup() {
   canvas.style = "border: 8px solid grey";
   centerCanvas();
   background(160,119,168);
-  var song = loadSound ("SpyroTheme.mp3");
+  song = loadSound ("SpyroTheme.mp3");
+  img = loadImage("images/spyro1.jpg");
+
 }
 
-function mousePressed()
-{
- song.play();
+function mousePressed() {
+  song.play();
 }
-
-
 function windowResized() {
   centerCanvas();
 }
 
 function draw() {
-textSize(30);
-text('DAT405!', 150,40);
+image(img, 0, 0, img.width+100, img.height+140 );
+textSize(30)
+text('DAT405!',150,40);
+fill(0)
+}
 
-
-
-
-
-
+function keyPressed(Space){
+  if (keyPressed());
+  img= loadImage("spyro2.jpg")
 }
